fcf29ae11c7ac22cf27226eb8aa2d4a8481a732e
kuvams <dimaborshpinsk@gmail.com>
2024-11-15 15:22:42 +0300

    complete 10 HW and styled it change some other styles

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,9 +2,17 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type StateType = typeof initState
+
+export const loadingReducer = (state = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+            case 'CHANGE_LOADING': {
+                return {
+                    ...state,
+                    isLoading: action.isLoading
+                }
+            }
 
         default:
             return state
@@ -20,3 +28,5 @@ export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
+
+type ActionType = LoadingActionType
