ca70482f2815b6cf9a046f964550b8b01f058a12
kuvams <dimaborshpinsk@gmail.com>
2024-11-26 14:30:56 +0300

    complete 11 HW and styled it

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HW10 from '../../hw10/HW10'
 import {Provider} from "react-redux";
 import store from "../../hw10/bll/store";
-// import HW11 from '../../hw11/HW11'
+import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
 // import HW13 from '../../hw13/HW13'
 // import HW14 from '../../hw14/HW14'
@@ -13,7 +13,7 @@ function JuniorPlus() {
         <div id={'hw5-page-junior-plus'}>
             <Provider store={store}>
                 <HW10/>
-                {/*<HW11 />*/}
+                <HW11 />
                 {/*<HW12 />*/}
                 {/*<HW13 />*/}
                 {/*<HW14 />*/}
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,16 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                maxWidth: "147px",
+                marginRight: "14px",
+                color: '#8B8B8B',
+                '& .MuiSlider-track': {
+                    color: '#00CC22'
+                },
+                '& .MuiSlider-thumb': {
+                    color: '#fff',
+                    border: '1px solid #00CC22'
+                }
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
